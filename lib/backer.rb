703b9164d3e0684e493dd1df06e94108f073641d
Daysha Jones <daysha.vu@icloud.com>
2021-03-19 17:23:54 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,16 +1,16 @@
 class Backer
-  attr_reader :name, :backed_projects
-  
-  def initialize(name)
-    @name = name
-    @backed_projects = []
-  end
-  
-  def back_project(project)
-    backed_projects << project
-  end
-  
-  def backed_projects
+
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def back_project(project)
+        ProjectBacker.new(project, self)
+    end
+
+    def backed_projects
         project_backer_instances = ProjectBacker.all.select do |project_backer|
             project_backer.backer == self
         end
@@ -19,5 +19,4 @@ class Backer
             pbi.project
         end
     end
-  
 end
