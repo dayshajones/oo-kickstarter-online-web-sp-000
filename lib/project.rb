12d048ba2530366d1504bfeb2308306b49844340
Daysha Jones <daysha.vu@icloud.com>
2021-03-19 17:24:56 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -10,14 +10,4 @@ class Project
         ProjectBacker.new(self, backer)
     end
 
-    def backers
-        project_backer_instances = ProjectBacker.all.select do |project_backer|
-            project_backer.project == self
-        end
-
-        project_backer_instances.map do |pbi|
-            pbi.backer
-        end
-    end
-
 end
